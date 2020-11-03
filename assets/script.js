2f8e147307e490e7bcdd9421d2a0fbedf6fb81c1
Christian Goldman <christian.goldman9638@gmail.com>
2020-11-03 16:51:51 -0600

    Displaying searched cities, working on displaying clickable cities

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,11 @@
-$("#searchBtn").on("submit", function(event) {
+$("#searchBtn").on("click", function(event) {
 
     event.preventDefault();
 
-    let searchedCity = $("#searchBtn").val();
+    let searchedCity = $("#citySearch").val();
     let APIkey = "da78f2587f0f29e343d3740e867a79e3";
-    let queryURL = "api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + APIkey;
+    let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + APIkey;
+
 
     $.ajax(
         {
@@ -13,5 +14,24 @@ $("#searchBtn").on("submit", function(event) {
         })
         .then(function(response) 
         {
-            console.log
+            $("#location").html(searchedCity);
+
+            let kTemp = response.main.temp;
+            let fTemp = Math.floor((kTemp - 273.15) * 1.8 + 32);
+            $("#temp").html(fTemp);
+
+            let hDitty = response.main.humidity;
+            $("#humidity").html(hDitty);
+
+            let windSpeed = response.wind.speed;
+            $("#windSpeed").html(windSpeed);
+        });
 });
+
+//http://localhost:52330/api.openweathermap.org/data/2.5/weather?q=Minneapolis&appid=da78f2587f0f29e343d3740e867a79e3
+// http://api.openweathermap.org/data/2.5/weather?q=Minneapolis&appid=da78f2587f0f29e343d3740e867a79e3
+
+// function myFunction(arg1, arg2) {
+//     console.log('arg1:' + arg1 + ',arg2:' + arg2);
+// }
+// myFunction('a', 'b', 'c', 'd');
