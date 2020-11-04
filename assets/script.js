bec70a72a60f40d4afcdf7824d57eab1d150d1e8
Christian Goldman <christian.goldman9638@gmail.com>
2020-11-04 12:23:02 -0600

    Searched city data is displaying, working on simplifying code

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,8 @@ $("#searchBtn").on("click", function(event) {
     let searchedCity = $("#citySearch").val();
     let APIkey = "da78f2587f0f29e343d3740e867a79e3";
     let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + APIkey;
-    // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key} 5 day search API
+    // below is the 5 day api
+    let queryFive = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&appid=" + APIkey; 
 
     $.ajax(
         {
@@ -25,13 +26,40 @@ $("#searchBtn").on("click", function(event) {
 
             let windSpeed = response.wind.speed;
             $("#windSpeed").html(windSpeed);
+
+            let lat = response.coord.lat;
+            let lon = response.coord.lon;
+            let queryUV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+            
+            // update 2nd api url within first ajax call.
+            // create a function for second and third ajax call 
+            $.ajax(
+                {
+                    url: queryUV,
+                    method: "GET"
+                })
+                .then(function(response){
+                    let uvIdx = response.value;
+                    console.log(uvIdx);
+                    $("#uvIndex").html(uvIdx);
+                });
         });
-});
+});   
 
-//http://localhost:52330/api.openweathermap.org/data/2.5/weather?q=Minneapolis&appid=da78f2587f0f29e343d3740e867a79e3
-// http://api.openweathermap.org/data/2.5/weather?q=Minneapolis&appid=da78f2587f0f29e343d3740e867a79e3
+function updateData(){
+    $("#location").html(searchedCity);
 
-// function myFunction(arg1, arg2) {
-//     console.log('arg1:' + arg1 + ',arg2:' + arg2);
-// }
-// myFunction('a', 'b', 'c', 'd');
+    let kTemp = response.main.temp;
+    let fTemp = Math.floor((kTemp - 273.15) * 1.8 + 32);
+    $("#temp").html(fTemp);
+
+    let hDitty = response.main.humidity;
+    $("#humidity").html(hDitty);
+
+    let windSpeed = response.wind.speed;
+    $("#windSpeed").html(windSpeed);
+
+    let lat = response.coord.lat;
+    let lon = response.coord.lon;
+    let queryUV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+}
